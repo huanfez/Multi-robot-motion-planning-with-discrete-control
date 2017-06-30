53a096fd30b4614962fc0cd3ffa80a60600a659e
Huanfei Zheng <zhenghuanfei@gmail.com>
2017-06-30 12:42:34 -0400

    Update obsPlot.m

--- obsPlot.m
+++ obsPlot.m
@@ -11,8 +11,8 @@ end
 % x1 = [[(x-w/2)';(x-w/2)'] [(x+w/2)';(x+w/2)']];
 % y1 = [[(y+w/2)';(y-w/2)'] [(y-w/2)';(y+w/2)']];
 
-x1 = [(x-w/2) (x-w/2); (x+w/2) (x+w/2)];
-y1 = [(y+w/2) (y-w/2); (y-w/2) (y+w/2)];
+x1 = [(x-w/2) (x-w/2) (x-w/2) (x+w/2) (x+w/2) (x-w/2); (x+w/2) (x+w/2) (x+w/2) (x+w/2) (x-w/2) (x-w/2)];
+y1 = [(y+w/2) (y-w/2) (y-w/2) (y-w/2) (y+w/2) (y+w/2); (y-w/2) (y+w/2) (y-w/2) (y+w/2) (y+w/2) (y-w/2)];
 
-h = plot(x1,y1,color,'LineWidth',LineWidth);
+h = plot(x1,y1,'color',color,'LineWidth',LineWidth);
 
